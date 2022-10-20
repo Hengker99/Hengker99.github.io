ee5da76d92b74c95ea171716394b155e7b98711f
mř.³⁴6⅞ <107789181+Hengker99@users.noreply.github.com>
2022-10-20 13:23:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const greeting = document.getElementById('greeting')
+let kelamin = prompt('pr/lk')
+let nama = prompt('masukan nama')
+let kelas = prompt('masukan kelas')
+let ket = ''
+let at = ''
+let it = ''
+switch(kelamin){
+  case 'lk':
+    ket = 'akang'
+    break
+  case 'pr':
+    at = 'teteh'
+    break
+  default :
+    it = 'ntah apa kelamin mu'
+}
+console.log(`selamat datang ${ket} ${nama} dari kelas ${kelas}`)
+alert(`selamat datang ${ket} ${nama} dari kelas ${kelas}`)
+greeting.innerHTML = 'selamat datang' + ' ' + ket + ' ' + at + ' ' + it + ' ' + nama + ' ' + kelas + ' ' + 'dari kelas'
